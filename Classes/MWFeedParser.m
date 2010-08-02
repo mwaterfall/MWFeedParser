mwaterfall/MWFeedParser

commit c106060c5c45d7cb08bb71084bfeb58d29f7d96e

Now supports RDF Site Summary (RSS) 1.0 specification.

Classes/MWFeedParser.m
@@ -166,6 +166,7 @@
 		// Parse!
 		feedParser = [[NSXMLParser alloc] initWithData:data];
 		feedParser.delegate = self;
+		[feedParser setShouldProcessNamespaces:YES];
 		[feedParser parse];
 		
 	}
@@ -283,6 +284,7 @@
 	// Determine feed type
 	if (feedType == FeedTypeUnknown) {
 		if ([elementName isEqualToString:@"rss"]) feedType = FeedTypeRSS; 
+		else if ([elementName isEqualToString:@"RDF"]) feedType = FeedTypeRSS1;
 		else if ([elementName isEqualToString:@"feed"]) feedType = FeedTypeAtom;
 		return;
 	}
@@ -290,6 +292,7 @@
 	// Entering new feed element
 	if (feedParseType != ParseTypeItemsOnly) {
 		if ((feedType == FeedTypeRSS  && [currentPath isEqualToString:@"/rss/channel"]) ||
+			(feedType == FeedTypeRSS1 && [currentPath isEqualToString:@"/RDF/channel"]) ||
 			(feedType == FeedTypeAtom && [currentPath isEqualToString:@"/feed"])) {
 			return;
 		}
@@ -297,6 +300,7 @@
 			
 	// Entering new item element
 	if ((feedType == FeedTypeRSS  && [currentPath isEqualToString:@"/rss/channel/item"]) ||
+		(feedType == FeedTypeRSS1 && [currentPath isEqualToString:@"/RDF/item"]) ||
 		(feedType == FeedTypeAtom && [currentPath isEqualToString:@"/feed/entry"])) {
 
 		// Send off feed info to delegate
@@ -366,6 +370,26 @@
 				
 				break;
 			}
+			case FeedTypeRSS1: {
+				
+				// Item
+				if (!processed) {
+					if ([currentPath isEqualToString:@"/RDF/item/title"]) { if (currentText.length > 0) item.title = currentText; processed = YES; }
+					else if ([currentPath isEqualToString:@"/RDF/item/link"]) { if (currentText.length > 0) item.link = currentText; processed = YES; }
+					else if ([currentPath isEqualToString:@"/RDF/item/description"]) { if (currentText.length > 0) item.summary = currentText; processed = YES; }
+					else if ([currentPath isEqualToString:@"/RDF/item/content:encoded"]) { if (currentText.length > 0) item.content = currentText; processed = YES; }
+					else if ([currentPath isEqualToString:@"/RDF/item/date"]) { if (currentText.length > 0) item.date = [self dateFromRFC3339String:currentText]; processed = YES; }
+				}
+				
+				// Info
+				if (!processed && feedParseType != ParseTypeItemsOnly) {
+					if ([currentPath isEqualToString:@"/RDF/channel/title"]) { if (currentText.length > 0) info.title = currentText; processed = YES; }
+					else if ([currentPath isEqualToString:@"/RDF/channel/description"]) { if (currentText.length > 0) info.summary = currentText; processed = YES; }
+					else if ([currentPath isEqualToString:@"/RDF/channel/link"]) { if (currentText.length > 0) info.link = currentText; processed = YES; }
+				}
+				
+				break;
+			}
 			case FeedTypeAtom: {
 				
 				// Item
@@ -394,7 +418,7 @@
 	
 	// If end of an item then tell delegate
 	if (!processed) {
-		if ((feedType == FeedTypeRSS && [elementName isEqualToString:@"item"]) ||
+		if (((feedType == FeedTypeRSS || feedType == FeedTypeRSS1) && [elementName isEqualToString:@"item"]) ||
 			(feedType == FeedTypeAtom && [elementName isEqualToString:@"entry"])) {
 			
 			// Dispatch item to delegate
@@ -406,6 +430,7 @@
 	// Check if the document has finished parsing and send off info if needed (i.e. there were no items)
 	if (!processed) {
 		if ((feedType == FeedTypeRSS && [elementName isEqualToString:@"rss"]) ||
+			(feedType == FeedTypeRSS1 && [elementName isEqualToString:@"RDF"]) ||
 			(feedType == FeedTypeAtom && [elementName isEqualToString:@"feed"])) {
 			
 			// Document ending so if we havent sent off feed info yet, do so

commit dcc892de293208ac7622cf9b9462ef21dfba9bcc

Fixed bug with namespaces and content:encoded element introduced in commit c106060.

Classes/MWFeedParser.m
@@ -166,7 +166,7 @@
 		// Parse!
 		feedParser = [[NSXMLParser alloc] initWithData:data];
 		feedParser.delegate = self;
-		[feedParser setShouldProcessNamespaces:YES];
+		//[feedParser setShouldProcessNamespaces:YES];
 		[feedParser parse];
 		
 	}
@@ -284,7 +284,7 @@
 	// Determine feed type
 	if (feedType == FeedTypeUnknown) {
 		if ([elementName isEqualToString:@"rss"]) feedType = FeedTypeRSS; 
-		else if ([elementName isEqualToString:@"RDF"]) feedType = FeedTypeRSS1;
+		else if ([elementName isEqualToString:@"rdf:RDF"]) feedType = FeedTypeRSS1;
 		else if ([elementName isEqualToString:@"feed"]) feedType = FeedTypeAtom;
 		return;
 	}
@@ -292,7 +292,7 @@
 	// Entering new feed element
 	if (feedParseType != ParseTypeItemsOnly) {
 		if ((feedType == FeedTypeRSS  && [currentPath isEqualToString:@"/rss/channel"]) ||
-			(feedType == FeedTypeRSS1 && [currentPath isEqualToString:@"/RDF/channel"]) ||
+			(feedType == FeedTypeRSS1 && [currentPath isEqualToString:@"/rdf:RDF/channel"]) ||
 			(feedType == FeedTypeAtom && [currentPath isEqualToString:@"/feed"])) {
 			return;
 		}
@@ -300,7 +300,7 @@
 			
 	// Entering new item element
 	if ((feedType == FeedTypeRSS  && [currentPath isEqualToString:@"/rss/channel/item"]) ||
-		(feedType == FeedTypeRSS1 && [currentPath isEqualToString:@"/RDF/item"]) ||
+		(feedType == FeedTypeRSS1 && [currentPath isEqualToString:@"/rdf:RDF/item"]) ||
 		(feedType == FeedTypeAtom && [currentPath isEqualToString:@"/feed/entry"])) {
 
 		// Send off feed info to delegate
@@ -374,18 +374,18 @@
 				
 				// Item
 				if (!processed) {
-					if ([currentPath isEqualToString:@"/RDF/item/title"]) { if (currentText.length > 0) item.title = currentText; processed = YES; }
-					else if ([currentPath isEqualToString:@"/RDF/item/link"]) { if (currentText.length > 0) item.link = currentText; processed = YES; }
-					else if ([currentPath isEqualToString:@"/RDF/item/description"]) { if (currentText.length > 0) item.summary = currentText; processed = YES; }
-					else if ([currentPath isEqualToString:@"/RDF/item/content:encoded"]) { if (currentText.length > 0) item.content = currentText; processed = YES; }
-					else if ([currentPath isEqualToString:@"/RDF/item/date"]) { if (currentText.length > 0) item.date = [self dateFromRFC3339String:currentText]; processed = YES; }
+					if ([currentPath isEqualToString:@"/rdf:RDF/item/title"]) { if (currentText.length > 0) item.title = currentText; processed = YES; }
+					else if ([currentPath isEqualToString:@"/rdf:RDF/item/link"]) { if (currentText.length > 0) item.link = currentText; processed = YES; }
+					else if ([currentPath isEqualToString:@"/rdf:RDF/item/description"]) { if (currentText.length > 0) item.summary = currentText; processed = YES; }
+					else if ([currentPath isEqualToString:@"/rdf:RDF/item/content:encoded"]) { if (currentText.length > 0) item.content = currentText; processed = YES; }
+					else if ([currentPath isEqualToString:@"/rdf:RDF/item/dc:date"]) { if (currentText.length > 0) item.date = [self dateFromRFC3339String:currentText]; processed = YES; }
 				}
 				
 				// Info
 				if (!processed && feedParseType != ParseTypeItemsOnly) {
-					if ([currentPath isEqualToString:@"/RDF/channel/title"]) { if (currentText.length > 0) info.title = currentText; processed = YES; }
-					else if ([currentPath isEqualToString:@"/RDF/channel/description"]) { if (currentText.length > 0) info.summary = currentText; processed = YES; }
-					else if ([currentPath isEqualToString:@"/RDF/channel/link"]) { if (currentText.length > 0) info.link = currentText; processed = YES; }
+					if ([currentPath isEqualToString:@"/rdf:RDF/channel/title"]) { if (currentText.length > 0) info.title = currentText; processed = YES; }
+					else if ([currentPath isEqualToString:@"/rdf:RDF/channel/description"]) { if (currentText.length > 0) info.summary = currentText; processed = YES; }
+					else if ([currentPath isEqualToString:@"/rdf:RDF/channel/link"]) { if (currentText.length > 0) info.link = currentText; processed = YES; }
 				}
 				
 				break;
@@ -430,7 +430,7 @@
 	// Check if the document has finished parsing and send off info if needed (i.e. there were no items)
 	if (!processed) {
 		if ((feedType == FeedTypeRSS && [elementName isEqualToString:@"rss"]) ||
-			(feedType == FeedTypeRSS1 && [elementName isEqualToString:@"RDF"]) ||
+			(feedType == FeedTypeRSS1 && [elementName isEqualToString:@"rdf:RDF"]) ||
 			(feedType == FeedTypeAtom && [elementName isEqualToString:@"feed"])) {
 			
 			// Document ending so if we havent sent off feed info yet, do so

MWFeedParser.xcodeproj/Michael.mode1v3
@@ -332,7 +332,7 @@
 										<key>_historyCapacity</key>
 										<integer>0</integer>
 										<key>bookmark</key>
-										<string>4C216A6C1206D67300C9E6E8</string>
+										<string>4C2945311206E20100605C9E</string>
 										<key>history</key>
 										<array>
 											<string>4C7F67BF11A007E300D6DF99</string>
@@ -344,14 +344,14 @@
 											<string>4C46617B11F878C700CB12BD</string>
 											<string>4C3EE70411FDDCCE0033C200</string>
 											<string>4C3EE72E11FDDDA20033C200</string>
-											<string>4C2169BA1206CBA000C9E6E8</string>
-											<string>4C2169BB1206CBA000C9E6E8</string>
 											<string>4C2169BC1206CBA000C9E6E8</string>
-											<string>4C216A301206D31E00C9E6E8</string>
-											<string>4C216A551206D52500C9E6E8</string>
-											<string>4C216A671206D66900C9E6E8</string>
-											<string>4C216A681206D66900C9E6E8</string>
-											<string>4C216A691206D66900C9E6E8</string>
+											<string>4C2944EE1206DA3B00605C9E</string>
+											<string>4C29452A1206E20100605C9E</string>
+											<string>4C29452B1206E20100605C9E</string>
+											<string>4C29452C1206E20100605C9E</string>
+											<string>4C29452D1206E20100605C9E</string>
+											<string>4C29452E1206E20100605C9E</string>
+											<string>4C29452F1206E20100605C9E</string>
 										</array>
 									</dict>
 									<key>SplitCount</key>
@@ -409,9 +409,9 @@
 			</array>
 			<key>TableOfContents</key>
 			<array>
-				<string>4C2169AF1206C8F200C9E6E8</string>
+				<string>4C2944D31206D77700605C9E</string>
 				<string>1CE0B1FE06471DED0097A5F4</string>
-				<string>4C2169B01206C8F200C9E6E8</string>
+				<string>4C2944D41206D77700605C9E</string>
 				<string>1CE0B20306471E060097A5F4</string>
 				<string>1CE0B20506471E060097A5F4</string>
 			</array>
@@ -549,10 +549,12 @@
 	<integer>5</integer>
 	<key>WindowOrderList</key>
 	<array>
-		<string>4C2169CF1206CC1200C9E6E8</string>
-		<string>4C2169D11206CC1200C9E6E8</string>
-		<string>1CD10A99069EF8BA00B06720</string>
+		<string>4C2945281206E1FB00605C9E</string>
+		<string>1C530D57069F1CE1000CFCEE</string>
+		<string>4C2944EA1206D82200605C9E</string>
+		<string>4C2944E11206D7B600605C9E</string>
 		<string>1C78EAAD065D492600B07095</string>
+		<string>1CD10A99069EF8BA00B06720</string>
 		<string>4CC895FE119F0CCB00ED61B6</string>
 		<string>/Users/Michael/My Work/d3i/Projects/MWFeedParser/MWFeedParser.xcodeproj</string>
 	</array>
@@ -634,7 +636,7 @@
 			<key>TableOfContents</key>
 			<array>
 				<string>4CC895FE119F0CCB00ED61B6</string>
-				<string>4C2169B11206C8F200C9E6E8</string>
+				<string>4C2944D51206D77700605C9E</string>
 				<string>1CD0528F0623707200166675</string>
 				<string>XCMainBuildResultsModuleGUID</string>
 			</array>
@@ -756,13 +758,13 @@
 			<key>TableOfContents</key>
 			<array>
 				<string>1CD10A99069EF8BA00B06720</string>
-				<string>4C2169C81206CC1200C9E6E8</string>
+				<string>4C2944D91206D7B600605C9E</string>
 				<string>1C162984064C10D400B95A72</string>
-				<string>4C2169C91206CC1200C9E6E8</string>
-				<string>4C2169CA1206CC1200C9E6E8</string>
-				<string>4C2169CB1206CC1200C9E6E8</string>
-				<string>4C2169CC1206CC1200C9E6E8</string>
-				<string>4C2169CD1206CC1200C9E6E8</string>
+				<string>4C2944DA1206D7B600605C9E</string>
+				<string>4C2944DB1206D7B600605C9E</string>
+				<string>4C2944DC1206D7B600605C9E</string>
+				<string>4C2944DD1206D7B600605C9E</string>
+				<string>4C2944DE1206D7B600605C9E</string>
 			</array>
 			<key>ToolbarConfiguration</key>
 			<string>xcode.toolbar.config.debugV3</string>
@@ -794,7 +796,7 @@
 										<key>PBXProjectModuleGUID</key>
 										<string>1CDD528C0622207200134675</string>
 										<key>PBXProjectModuleLabel</key>
-										<string>MWFeedParser.m</string>
+										<string></string>
 										<key>StatusBarVisibility</key>
 										<true/>
 									</dict>
@@ -803,7 +805,7 @@
 										<key>Frame</key>
 										<string>{{0, 0}, {1179, 376}}</string>
 										<key>RubberWindowFrame</key>
-										<string>142 94 1179 736 0 0 1440 878 </string>
+										<string>403 293 1179 736 0 0 1920 1178 </string>
 									</dict>
 									<key>Module</key>
 									<string>PBXNavigatorGroup</string>
@@ -829,7 +831,7 @@
 								<key>Frame</key>
 								<string>{{0, 381}, {1179, 314}}</string>
 								<key>RubberWindowFrame</key>
-								<string>142 94 1179 736 0 0 1440 878 </string>
+								<string>403 293 1179 736 0 0 1920 1178 </string>
 							</dict>
 							<key>Module</key>
 							<string>PBXProjectFindModule</string>
@@ -852,13 +854,13 @@
 			<key>TableOfContents</key>
 			<array>
 				<string>1C530D57069F1CE1000CFCEE</string>
-				<string>4C3EE6FC11FDDA0D0033C200</string>
-				<string>4C3EE6FD11FDDA0D0033C200</string>
+				<string>4C2945071206E07A00605C9E</string>
+				<string>4C2945081206E07A00605C9E</string>
 				<string>1CDD528C0622207200134675</string>
 				<string>1CD0528E0623707200166675</string>
 			</array>
 			<key>WindowString</key>
-			<string>142 94 1179 736 0 0 1440 878 </string>
+			<string>403 293 1179 736 0 0 1920 1178 </string>
 			<key>WindowToolGUID</key>
 			<string>1C530D57069F1CE1000CFCEE</string>
 			<key>WindowToolIsVisible</key>
@@ -918,7 +920,7 @@
 			<key>TableOfContents</key>
 			<array>
 				<string>1C78EAAD065D492600B07095</string>
-				<string>4C2169CE1206CC1200C9E6E8</string>
+				<string>4C2944DF1206D7B600605C9E</string>
 				<string>1C78EAAC065D492600B07095</string>
 			</array>
 			<key>ToolbarConfiguration</key>
@@ -1065,18 +1067,18 @@
 			<string>743 379 452 308 0 0 1280 1002 </string>
 		</dict>
 		<dict>
+			<key>FirstTimeWindowDisplayed</key>
+			<false/>
 			<key>Identifier</key>
 			<string>windowTool.breakpoints</string>
 			<key>IsVertical</key>
-			<integer>0</integer>
+			<false/>
 			<key>Layout</key>
 			<array>
 				<dict>
 					<key>Dock</key>
 					<array>
 						<dict>
-							<key>BecomeActive</key>
-							<integer>1</integer>
 							<key>ContentConfiguration</key>
 							<dict>
 								<key>PBXBottomSmartGroupGIDs</key>
@@ -1118,7 +1120,7 @@
 								<key>PBXTopSmartGroupGIDs</key>
 								<array/>
 								<key>XCIncludePerspectivesSwitch</key>
-								<integer>0</integer>
+								<false/>
 							</dict>
 							<key>GeometryConfiguration</key>
 							<dict>
@@ -1130,7 +1132,7 @@
 									<real>168</real>
 								</array>
 								<key>RubberWindowFrame</key>
-								<string>315 424 744 409 0 0 1440 878 </string>
+								<string>62 722 744 409 0 0 1920 1178 </string>
 							</dict>
 							<key>Module</key>
 							<string>PBXSmartGroupTreeModule</string>
@@ -1138,6 +1140,8 @@
 							<string>185pt</string>
 						</dict>
 						<dict>
+							<key>BecomeActive</key>
+							<true/>
 							<key>ContentConfiguration</key>
 							<dict>
 								<key>PBXProjectModuleGUID</key>
@@ -1150,7 +1154,7 @@
 								<key>Frame</key>
 								<string>{{190, 0}, {554, 368}}</string>
 								<key>RubberWindowFrame</key>
-								<string>315 424 744 409 0 0 1440 878 </string>
+								<string>62 722 744 409 0 0 1920 1178 </string>
 							</dict>
 							<key>Module</key>
 							<string>XCDetailModule</string>
@@ -1174,22 +1178,22 @@
 				<string>XCDetailModule</string>
 			</array>
 			<key>StatusbarIsVisible</key>
-			<integer>1</integer>
+			<true/>
 			<key>TableOfContents</key>
 			<array>
-				<string>1CDDB66807F98D9800BB5817</string>
-				<string>1CDDB66907F98D9800BB5817</string>
+				<string>4C2945281206E1FB00605C9E</string>
+				<string>4C2945291206E1FB00605C9E</string>
 				<string>1CE0B1FE06471DED0097A5F4</string>
 				<string>1CA1AED706398EBD00589147</string>
 			</array>
 			<key>ToolbarConfiguration</key>
 			<string>xcode.toolbar.config.breakpointsV3</string>
 			<key>WindowString</key>
-			<string>315 424 744 409 0 0 1440 878 </string>
+			<string>62 722 744 409 0 0 1920 1178 </string>
 			<key>WindowToolGUID</key>
-			<string>1CDDB66807F98D9800BB5817</string>
+			<string>4C2945281206E1FB00605C9E</string>
 			<key>WindowToolIsVisible</key>
-			<integer>1</integer>
+			<false/>
 		</dict>
 		<dict>
 			<key>Identifier</key>

MWFeedParser.xcodeproj/Michael.pbxuser
@@ -9,7 +9,7 @@
 	};
 	1D3623250D0F684500981E51 /* MWFeedParserAppDelegate.m */ = {
 		uiCtxt = {
-			sepNavIntBoundsRect = "{{0, 0}, {1544, 1041}}";
+			sepNavIntBoundsRect = "{{0, 0}, {1544, 1016}}";
 			sepNavSelRange = "{0, 0}";
 			sepNavVisRange = "{0, 897}";
 		};
@@ -35,6 +35,30 @@
 			4CC895F1119F0CC900ED61B6 /* MWFeedParser */,
 		);
 		perUserDictionary = {
+			"PBXConfiguration.PBXBreakpointsDataSource.v1:1CA1AED706398EBD00589147" = {
+				PBXFileTableDataSourceColumnSortingDirectionKey = "-1";
+				PBXFileTableDataSourceColumnSortingKey = PBXBreakpointsDataSource_BreakpointID;
+				PBXFileTableDataSourceColumnWidthsKey = (
+					20,
+					20,
+					198,
+					20,
+					99,
+					99,
+					29,
+					20,
+				);
+				PBXFileTableDataSourceColumnsKey = (
+					PBXBreakpointsDataSource_ActionID,
+					PBXBreakpointsDataSource_TypeID,
+					PBXBreakpointsDataSource_BreakpointID,
+					PBXBreakpointsDataSource_UseID,
+					PBXBreakpointsDataSource_LocationID,
+					PBXBreakpointsDataSource_ConditionID,
+					PBXBreakpointsDataSource_IgnoreCountID,
+					PBXBreakpointsDataSource_ContinueID,
+				);
+			};
 			PBXConfiguration.PBXFileTableDataSource3.PBXFileTableDataSource = {
 				PBXFileTableDataSourceColumnSortingDirectionKey = "-1";
 				PBXFileTableDataSourceColumnSortingKey = PBXFileDataSource_Filename_ColumnID;
@@ -91,19 +115,19 @@
 					PBXFileDataSource_Warnings_ColumnID,
 				);
 			};
-			PBXPerProjectTemplateStateSaveDate = 302434539;
-			PBXWorkspaceStateSaveDate = 302434539;
+			PBXPerProjectTemplateStateSaveDate = 302438260;
+			PBXWorkspaceStateSaveDate = 302438260;
 		};
 		perUserProjectItems = {
-			4C2169BA1206CBA000C9E6E8 /* PBXTextBookmark */ = 4C2169BA1206CBA000C9E6E8 /* PBXTextBookmark */;
-			4C2169BB1206CBA000C9E6E8 /* PBXTextBookmark */ = 4C2169BB1206CBA000C9E6E8 /* PBXTextBookmark */;
 			4C2169BC1206CBA000C9E6E8 /* PBXTextBookmark */ = 4C2169BC1206CBA000C9E6E8 /* PBXTextBookmark */;
-			4C216A301206D31E00C9E6E8 /* PBXTextBookmark */ = 4C216A301206D31E00C9E6E8 /* PBXTextBookmark */;
-			4C216A551206D52500C9E6E8 /* PBXTextBookmark */ = 4C216A551206D52500C9E6E8 /* PBXTextBookmark */;
-			4C216A671206D66900C9E6E8 /* PBXTextBookmark */ = 4C216A671206D66900C9E6E8 /* PBXTextBookmark */;
-			4C216A681206D66900C9E6E8 /* PBXTextBookmark */ = 4C216A681206D66900C9E6E8 /* PBXTextBookmark */;
-			4C216A691206D66900C9E6E8 /* PBXTextBookmark */ = 4C216A691206D66900C9E6E8 /* PBXTextBookmark */;
-			4C216A6C1206D67300C9E6E8 /* PBXTextBookmark */ = 4C216A6C1206D67300C9E6E8 /* PBXTextBookmark */;
+			4C2944EE1206DA3B00605C9E /* PBXTextBookmark */ = 4C2944EE1206DA3B00605C9E /* PBXTextBookmark */;
+			4C29452A1206E20100605C9E /* PBXTextBookmark */ = 4C29452A1206E20100605C9E /* PBXTextBookmark */;
+			4C29452B1206E20100605C9E /* PBXTextBookmark */ = 4C29452B1206E20100605C9E /* PBXTextBookmark */;
+			4C29452C1206E20100605C9E /* PBXTextBookmark */ = 4C29452C1206E20100605C9E /* PBXTextBookmark */;
+			4C29452D1206E20100605C9E /* PBXTextBookmark */ = 4C29452D1206E20100605C9E /* PBXTextBookmark */;
+			4C29452E1206E20100605C9E /* PBXTextBookmark */ = 4C29452E1206E20100605C9E /* PBXTextBookmark */;
+			4C29452F1206E20100605C9E /* PBXTextBookmark */ = 4C29452F1206E20100605C9E /* PBXTextBookmark */;
+			4C2945311206E20100605C9E /* PBXTextBookmark */ = 4C2945311206E20100605C9E /* PBXTextBookmark */;
 			4C3EE70411FDDCCE0033C200 /* PBXTextBookmark */ = 4C3EE70411FDDCCE0033C200 /* PBXTextBookmark */;
 			4C3EE72E11FDDDA20033C200 /* PBXTextBookmark */ = 4C3EE72E11FDDDA20033C200 /* PBXTextBookmark */;
 			4C4660DB11F8614100CB12BD /* PBXTextBookmark */ = 4C4660DB11F8614100CB12BD /* PBXTextBookmark */;
@@ -125,26 +149,6 @@
 			sepNavVisRange = "{0, 360}";
 		};
 	};
-	4C2169BA1206CBA000C9E6E8 /* PBXTextBookmark */ = {
-		isa = PBXTextBookmark;
-		fRef = 4CC89604119F0CED00ED61B6 /* MWFeedParser.m */;
-		name = "MWFeedParser.m: 39";
-		rLen = 0;
-		rLoc = 963;
-		rType = 0;
-		vrLen = 2193;
-		vrLoc = 0;
-	};
-	4C2169BB1206CBA000C9E6E8 /* PBXTextBookmark */ = {
-		isa = PBXTextBookmark;
-		fRef = 1D3623250D0F684500981E51 /* MWFeedParserAppDelegate.m */;
-		name = "MWFeedParserAppDelegate.m: 1";
-		rLen = 0;
-		rLoc = 0;
-		rType = 0;
-		vrLen = 897;
-		vrLoc = 0;
-	};
 	4C2169BC1206CBA000C9E6E8 /* PBXTextBookmark */ = {
 		isa = PBXTextBookmark;
 		fRef = 4CC89610119F0D0C00ED61B6 /* RootViewController.h */;
@@ -155,7 +159,17 @@
 		vrLen = 376;
 		vrLoc = 0;
 	};
-	4C216A301206D31E00C9E6E8 /* PBXTextBookmark */ = {
+	4C2944EE1206DA3B00605C9E /* PBXTextBookmark */ = {
+		isa = PBXTextBookmark;
+		fRef = 1D3623250D0F684500981E51 /* MWFeedParserAppDelegate.m */;
+		name = "MWFeedParserAppDelegate.m: 1";
+		rLen = 0;
+		rLoc = 0;
+		rType = 0;
+		vrLen = 897;
+		vrLoc = 0;
+	};
+	4C29452A1206E20100605C9E /* PBXTextBookmark */ = {
 		isa = PBXTextBookmark;
 		fRef = 4CC89605119F0CED00ED61B6 /* MWFeedInfo.h */;
 		name = "MWFeedInfo.h: 11";
@@ -165,27 +179,7 @@
 		vrLen = 436;
 		vrLoc = 0;
 	};
-	4C216A551206D52500C9E6E8 /* PBXTextBookmark */ = {
-		isa = PBXTextBookmark;
-		fRef = 4CC89607119F0CED00ED61B6 /* MWFeedItem.h */;
-		name = "MWFeedItem.h: 11";
-		rLen = 0;
-		rLoc = 229;
-		rType = 0;
-		vrLen = 622;
-		vrLoc = 0;
-	};
-	4C216A671206D66900C9E6E8 /* PBXTextBookmark */ = {
-		isa = PBXTextBookmark;
-		fRef = 4CC89608119F0CED00ED61B6 /* MWFeedItem.m */;
-		name = "MWFeedItem.m: 53";
-		rLen = 0;
-		rLoc = 1529;
-		rType = 0;
-		vrLen = 1666;
-		vrLoc = 0;
-	};
-	4C216A681206D66900C9E6E8 /* PBXTextBookmark */ = {
+	4C29452B1206E20100605C9E /* PBXTextBookmark */ = {
 		isa = PBXTextBookmark;
 		fRef = 4CC89606119F0CED00ED61B6 /* MWFeedInfo.m */;
 		name = "MWFeedInfo.m: 21";
@@ -195,17 +189,47 @@
 		vrLen = 1327;
 		vrLoc = 0;
 	};
-	4C216A691206D66900C9E6E8 /* PBXTextBookmark */ = {
+	4C29452C1206E20100605C9E /* PBXTextBookmark */ = {
+		isa = PBXTextBookmark;
+		fRef = 4CC89607119F0CED00ED61B6 /* MWFeedItem.h */;
+		name = "MWFeedItem.h: 11";
+		rLen = 0;
+		rLoc = 229;
+		rType = 0;
+		vrLen = 622;
+		vrLoc = 0;
+	};
+	4C29452D1206E20100605C9E /* PBXTextBookmark */ = {
+		isa = PBXTextBookmark;
+		fRef = 4CC89608119F0CED00ED61B6 /* MWFeedItem.m */;
+		name = "MWFeedItem.m: 53";
+		rLen = 0;
+		rLoc = 1529;
+		rType = 0;
+		vrLen = 1420;
+		vrLoc = 0;
+	};
+	4C29452E1206E20100605C9E /* PBXTextBookmark */ = {
+		isa = PBXTextBookmark;
+		fRef = 4CC89604119F0CED00ED61B6 /* MWFeedParser.m */;
+		name = "MWFeedParser.m: 193";
+		rLen = 0;
+		rLoc = 4593;
+		rType = 0;
+		vrLen = 1031;
+		vrLoc = 4170;
+	};
+	4C29452F1206E20100605C9E /* PBXTextBookmark */ = {
 		isa = PBXTextBookmark;
 		fRef = 4CC89611119F0D0C00ED61B6 /* RootViewController.m */;
 		name = "RootViewController.m: 1";
 		rLen = 0;
 		rLoc = 0;
 		rType = 0;
-		vrLen = 2151;
+		vrLen = 1296;
 		vrLoc = 0;
 	};
-	4C216A6C1206D67300C9E6E8 /* PBXTextBookmark */ = {
+	4C2945311206E20100605C9E /* PBXTextBookmark */ = {
 		isa = PBXTextBookmark;
 		fRef = 4CC89611119F0D0C00ED61B6 /* RootViewController.m */;
 		name = "RootViewController.m: 1";
@@ -371,38 +395,38 @@
 	};
 	4CC89604119F0CED00ED61B6 /* MWFeedParser.m */ = {
 		uiCtxt = {
-			sepNavIntBoundsRect = "{{0, 0}, {1544, 8047}}";
-			sepNavSelRange = "{963, 0}";
-			sepNavVisRange = "{0, 2193}";
+			sepNavIntBoundsRect = "{{0, 0}, {1097, 9230}}";
+			sepNavSelRange = "{4593, 0}";
+			sepNavVisRange = "{4170, 1031}";
 			sepNavWindowFrame = "{{-1425, 217}, {1230, 874}}";
 		};
 	};
 	4CC89605119F0CED00ED61B6 /* MWFeedInfo.h */ = {
 		uiCtxt = {
-			sepNavIntBoundsRect = "{{0, 0}, {1544, 1018}}";
+			sepNavIntBoundsRect = "{{0, 0}, {873, 654}}";
 			sepNavSelRange = "{228, 0}";
 			sepNavVisRange = "{0, 436}";
 		};
 	};
 	4CC89606119F0CED00ED61B6 /* MWFeedInfo.m */ = {
 		uiCtxt = {
-			sepNavIntBoundsRect = "{{0, 0}, {1544, 1041}}";
+			sepNavIntBoundsRect = "{{0, 0}, {873, 676}}";
 			sepNavSelRange = "{543, 0}";
 			sepNavVisRange = "{0, 1327}";
 		};
 	};
 	4CC89607119F0CED00ED61B6 /* MWFeedItem.h */ = {
 		uiCtxt = {
-			sepNavIntBoundsRect = "{{0, 0}, {1544, 1041}}";
+			sepNavIntBoundsRect = "{{0, 0}, {873, 654}}";
 			sepNavSelRange = "{229, 0}";
 			sepNavVisRange = "{0, 622}";
 		};
 	};
 	4CC89608119F0CED00ED61B6 /* MWFeedItem.m */ = {
 		uiCtxt = {
-			sepNavIntBoundsRect = "{{0, 0}, {1544, 1041}}";
+			sepNavIntBoundsRect = "{{0, 0}, {873, 767}}";
 			sepNavSelRange = "{1529, 0}";
-			sepNavVisRange = "{0, 1666}";
+			sepNavVisRange = "{0, 1420}";
 		};
 	};
 	4CC8960D119F0CFB00ED61B6 /* NSString+XMLEntities.h */ = {