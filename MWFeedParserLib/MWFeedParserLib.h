83cfd890f88014e45e5566c42f43f6cddfec44ea
Hubert SARRET <hubert.sarret@gmail.com>
2013-04-11 18:19:16 +0200

    Added a static library target

---
+++ MWFeedParserLib/MWFeedParserLib.h
@@ -0,0 +1,13 @@
+//
+//  MWFeedParserLib.h
+//  MWFeedParserLib
+//
+//  Created by Hubert SARRET on 11/04/13.
+//  Copyright (c) 2013 Michael Waterfall. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MWFeedParserLib : NSObject
+
+@end
