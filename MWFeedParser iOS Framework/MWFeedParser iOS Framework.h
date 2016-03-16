1d31a3e6da52e8dbd24e37925c6673901f078506
tiagomartinho <tiago.martinho@xpeppers.com>
2016-03-16 21:33:29 +0100

    defines MWFeedParser framework for iOS

---
+++ MWFeedParser iOS Framework/MWFeedParser iOS Framework.h
@@ -0,0 +1,19 @@
+//
+//  MWFeedParser iOS Framework.h
+//  MWFeedParser iOS Framework
+//
+//  Created by Tiago Martinho on 16/03/16.
+//  Copyright © 2016 Michael Waterfall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MWFeedParser iOS Framework.
+FOUNDATION_EXPORT double MWFeedParser_iOS_FrameworkVersionNumber;
+
+//! Project version string for MWFeedParser iOS Framework.
+FOUNDATION_EXPORT const unsigned char MWFeedParser_iOS_FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MWFeedParser_iOS_Framework/PublicHeader.h>
+
+
