bb24f7d21ea7b026bcdab630267e8a7db6f7cb59
tiagomartinho <tiago.martinho@xpeppers.com>
2016-03-16 22:09:57 +0100

    defines tvOS Framework target

---
+++ MWFeedParser tvOS Framework/MWFeedParser tvOS Framework.h
@@ -0,0 +1,19 @@
+//
+//  MWFeedParser tvOS Framework.h
+//  MWFeedParser tvOS Framework
+//
+//  Created by Tiago Martinho on 16/03/16.
+//  Copyright © 2016 Michael Waterfall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MWFeedParser tvOS Framework.
+FOUNDATION_EXPORT double MWFeedParser_tvOS_FrameworkVersionNumber;
+
+//! Project version string for MWFeedParser tvOS Framework.
+FOUNDATION_EXPORT const unsigned char MWFeedParser_tvOS_FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MWFeedParser_tvOS_Framework/PublicHeader.h>
+
+
